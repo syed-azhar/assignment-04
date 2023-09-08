dd07b3ebc63f7755838e175392ce4e0163f754b7
syed-azhar <93089810+syed-azhar@users.noreply.github.com>
2023-09-08 00:44:26 -0700

    javascript-assignment-04

---
+++ app.js
@@ -0,0 +1,189 @@
+// 3. Write a program to print numeric counting from 1 to 10.
+
+
+// for(var i=1;i <= 10; i++){
+//     console.log(i)
+// }
+
+// Done 
+
+// 4. Write a program to print multiplication table of any
+// number using for loop. Table number & length should be
+// taken as an input from user.
+
+
+// var table =2;
+// for (var i =1;i<=15;i++){
+//     console.log("Table ==== >" ,table + " x " + i + "=" + table * i)
+// }
+
+
+// 5. Write a program to print items of the following array
+// using for loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”,
+// “strawberry”]
+
+
+
+
+// 7. You have an array
+// A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”]
+// Write a program to enable “search by user input” in an
+// array.
+// After searching, prompt the user whether the given item is
+// found in the list or not. Example:
+
+
+
+
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// var searchItem = prompt("enter your item")
+
+// if(A.includes(searchItem)){
+//     alert("this itme is available")
+// }else{
+//     alert("this is not available")
+// }
+
+
+
+// Write a program to identify the largest number in the
+// given array.
+
+
+// var numbers = [444,555,666,777,888,999];
+
+// var maxNumbers = numbers[0];
+
+// for(i=0;i < numbers.length;i++){
+//     if(numbers[i]> maxNumbers){
+//         maxNumbers = numbers[i]
+//     }
+// }
+// console.log("The largest num is ",maxNumbers)
+
+
+// 9. Write a program to identify the smallest number in the
+// given array.
+// var smallest = [24, 53, 78, 91, 12]
+
+// var minNumber = smallest[3]
+
+// for(var i = 0;i < smallest.length;i++){
+//     if(smallest[i] < minNumber){
+//         minNumber = smallest[i]
+//     }
+
+// }
+
+// console.log(minNumber)
+
+
+
+// var fruits = ["apple","banana","mango","orange"];
+
+// for(var i =0;i < fruits.length;i++){
+//     console.log(fruits)
+// }
+
+
+// 6. Generate the following series in your browser. See
+// example output.
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+// b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+// c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+// d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+// e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+
+
+
+// var series = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+// document.write("Counting series  "  ,  series)
+
+// var reverse = series.reverse()
+
+// document.write("Reverse Counting   " ,reverse)
+
+
+// var user =prompt ("enter odd and even")
+
+// if(user % 2 === 0){
+//     alert("this is even")
+
+// }else{
+//     alert("this is odd");
+// }
+
+
+// 2. Write a program to take a user input about his favorite
+// mobile phone model. Find and display the length of user
+// input in your browser
+
+
+// var favtMob = prompt ("Enter your favt mobile phonw")
+
+// console.log("my favt phone is ", favtMob,"length of string is > ",favtMob.length)
+
+
+// 3. Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser 
+
+// var find = "pakistan"
+
+// var result = find.indexOf("n")
+
+// console.log("index of n is >>", result)
+
+
+// 4. Write a program to find the last index of letter “l” in the
+// word “Hello World” and display the result in your browser.
+
+// var find = "hello world"
+
+// var result = find.lastIndexOf("l");
+
+// console.log("Last index of l is ",result)
+
+
+// 5. Write a program to find the character at 3rd index in the
+// word “Pakistani” and display the result in your browser.
+
+
+// var word = "pakistan"
+
+// var result = word.charAt(3)
+
+// console.log("the 3rd character of pakistan is ",result)
+
+
+
+// 7. Write a program to replace the “Hyder” to “Islam” in the
+// word “Hyderabad” and display the result in your browser.
+
+
+// var word = "hyderabad"
+
+// var result = word.replace("hydera","islama")
+
+// console.log(result)
+
+
+// 9. Write a program that converts a string “472” to a number
+// 472. Display the values & types in your browser.
+
+
+// var stringIntonumber = "321"
+
+// var result = parseInt(stringIntonumber)
+
+// console.log(typeof(result))
+
+
+
+
+
+
+
+
